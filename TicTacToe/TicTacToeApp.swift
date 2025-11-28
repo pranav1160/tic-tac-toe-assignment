f1a8c9fb1a98664ff80ec517f86f8863fb718d87
Pranav <pranav1160ly@gmail.com>
2025-11-29 01:01:16 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Pranav on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
